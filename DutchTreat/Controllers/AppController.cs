21656437cedf9a8e1046f5d892e9f0f97a15602f
antonio jr <antonio.jr@ivia.com.br>
2017-11-10 15:42:17 -0300

    Add project files.

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,44 @@
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {                     
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {       
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMessage("antonio.jr@outlook.com", "Teste", "corpo da mensagem");
+                ViewBag.UserMesssage = "Mail Sent";
+                ModelState.Clear();
+            } 
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About";
+            return View();
+        }
+    }
+}
---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying")
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("you clicked on " + $(this).text());
+    });
+});
+    
